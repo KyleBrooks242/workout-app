4b5994236d87abad83c6941669018f716f2381c1
Kyle Brooks <brookska@g.cofc.edu>
2022-09-16 23:04:43 -0400

    Add some stubbed components

---
+++ workout-app/src/components/LoginComponent.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const LoginComponent = () => {
+    return (
+        <p>Hello, I will be the login page at some point!</p>
+    )
+}
---
+++ workout-app/src/components/MenuComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {LogoComponent} from "./LogoComponent";
+export const MenuComponent = () => {
+    return (
+        <div>
+            <LogoComponent/>
+        </div>
+)
+}
